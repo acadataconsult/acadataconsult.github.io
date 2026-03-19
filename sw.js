56b3005363587d3c8ce62695fb8d6e8d61fb6362
acadataconsult <acadataconsult@gmail.com>
2026-03-19 00:53:42 +0000

    PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "acadata-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./logo.png",
+  "./hero2.png",
+  "./hero3.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
